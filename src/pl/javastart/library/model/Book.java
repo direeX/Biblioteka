b5689b3408ed321565ae58d32cde6cc8be294862
direeX <dominik.obara7@gmail.com>
2023-02-02 17:09:08 +0100

    Library

---
+++ src/pl/javastart/library/model/Book.java
@@ -0,0 +1,48 @@
+package pl.javastart.library.model;
+
+public class Book extends Publication{
+
+   private String author;
+   private int pages;
+   private String isbn;
+
+    public Book(String title, String author, int year, int pages, String publisher, String isbn) {
+        super(title, publisher, year);
+        this.author = author;
+        this.pages = pages;
+        this.isbn = isbn;
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+@Override
+    public void printInfo(){
+        String info = "Tytuł: " + getTitle() + "; Autor: " + author + "; Rok wydania: " + getYear() + "; Liczba stron: " + pages + "; Wydawnictwo: " + getPublisher();
+        if(isbn != null){
+            info = info + "; ISBN: " + isbn;
+        }
+        System.out.println(info);
+    }
+}
